0c67a26c626f6a4737c6b65e8c62ab4686871b68
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-17 02:38:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,12 +3,21 @@
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text
 require 'nokogiri' 
 require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry      
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
+  
+  projects 
+
 end 
 create_project_hash
