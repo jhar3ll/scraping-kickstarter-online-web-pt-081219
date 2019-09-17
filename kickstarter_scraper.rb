323d9f0e27bf44af91fea194b094cfa6bb5cfc06
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-17 02:09:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry        # projects: kickstarter.css("li.project.grid_4")
-end
+end                  # title: project.css("h2.bbcard_name strong a").text
 
 create_project_hash
