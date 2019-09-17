1db017fa60918779feb50d02f9af517a7adfd643
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-17 02:23:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,8 @@
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
-
-
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta").text
 require 'nokogiri' 
 require 'pry'
 
